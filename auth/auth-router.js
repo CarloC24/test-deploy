fff3dc744fa5d19f122ae08e98bb92e3d3ea473d
CarloC24 <carlo.clamucha@gmail.com>
2019-05-20 17:16:59 -0500

    added fix?

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const secrets = require("../config/secrets");
 const Users = require("../user/users-model");
 
